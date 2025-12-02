f44ebf04ed6befb23d45cb35b13e3300cbf3475f
Kateryna Koliada <6parkseolhwa6@gmail.com>
2025-12-03 00:11:21 +0900

    Initial commit

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import css from "./Modal.module.css";
+import { createPortal } from "react-dom";
+
+interface ModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+function Modal({ onClose, children }: ModalProps) {
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown);
+
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "auto";
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClick}
+    >
+      <div className={css.modal}>{children}</div>
+    </div>,
+    document.body
+  );
+}
+
+export default Modal;
---
+++ components/NoteList/NoteList.tsx
@@ -0,0 +1,50 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { Note } from "../../types/note";
+import css from "./NoteList.module.css";
+import { deleteNote } from "@/lib/api";
+import Link from "next/link";
+
+interface NoteListProps {
+  notes: Note[];
+}
+
+function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (taskId: string) => deleteNote(taskId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
+
+  const handleDelete = (noteId: string) => {
+    mutation.mutate(noteId);
+  };
+
+  return (
+    <ul className={css.list}>
+      {notes.map((note) => {
+        return (
+          <li key={note.id} className={css.listItem}>
+            <h2 className={css.title}>{note.title}</h2>
+            <p className={css.content}>{note.content}</p>
+            <div className={css.footer}>
+              <span className={css.tag}>{note.tag}</span>
+              <Link href={`/notes/${note.id}`} className={css.viewButton}>
+                View Details
+              </Link>
+              <button
+                className={css.button}
+                onClick={() => handleDelete(note.id)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default NoteList;
